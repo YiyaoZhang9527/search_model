9b19275406c06565bcf95b785547fec8d3ee4329
张晶 <zhangjing@zhangjingdeMacBook-Pro.local>
2020-12-29 15:29:31 +0800

    bate0.1

--- push_project.sh
+++ push_project.sh
@@ -1,6 +1,6 @@
 git init
 git add .
 git commit -m "bate0.1"
-git remote add origin https://github.com/YiyaoZhang9527/search_model_learning_materials.git
+git remote add origin https://github.com/YiyaoZhang9527/search_model.git
 git push -u origin main master   
 

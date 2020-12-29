4f0c18de203e622c9b6ec709a72a65473889025d
张晶 <zhangjing@zhangjingdeMacBook-Pro.local>
2020-12-29 15:28:38 +0800

    bate0.1

--- downloads_data_from_lagou.sh
+++
@@ -1,1 +0,0 @@
-rsync -avP -e 'ssh -p 9898' ali:/root/crawlers_001/拉勾网/jobs_table_from_lagou.csv Documents/算法笔记/000_数据分析demo/拉勾网数据分析/jbs_table_from_lagou.csv
--- push_project.sh
+++ push_project.sh
@@ -1,3 +1,6 @@
-rsync -avP ~/Documents/算法笔记/000_数据分析demo/孟哥版/ root@124.71.142.97:~/孟哥版/
-
+git init
+git add .
+git commit -m "bate0.1"
+git remote add origin https://github.com/YiyaoZhang9527/search_model_learning_materials.git
+git push -u origin main master   
 
